57ee2c89f9a8ec482cb859297c6296506edd2d54
rodeatharva04 <123895899+rodeatharva04@users.noreply.github.com>
2025-12-30 16:45:48 +0530

    Update settings.py

--- noteshare/settings.py
+++ noteshare/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key')
 
